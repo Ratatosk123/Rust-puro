7951643e0c66b55e0630abdaed45208b75f42825
Ratatosk123 <allanmaiagondim.amg@gmail.com>
2025-08-15 20:31:51 -0300

    Aprendendo Variantes e suas Funções Rust

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,44 @@
-struct Usuario {
-	ativo: bool,
-	nome: String,
-	email: String,
-	contagem_logins: u64,
+enum Mensagem {
+	Sair,					// Varaiante sem dados 
+	Mover { x: i32, y:i32}, //Variante com uma struct anônima
+	Escrever(String),		// Variante com uma String 
+	MudarCor(i32, i32, i32), //Variante com uma tupla de 3 inteiros 
 }
 
-//Bloco de implementação para a struct usuário
-impl Usuario {
-	//Este é um método. Ele "pega emprestado" o objeto de forma imutável.
-	//'&self' é uma referência à instância da struct na qual o método foi chamado.
-	fn descrever(&self) -> String {
-		//format! é como println!, mas retorna a String em vez de imprimir.
-		format!("{} <{}> - Ativo: {}", self.nome, self.email, self.ativo)
-	}
-	//Este método "pega emprestado" o objeto de forma mútavel.
-	fn desativar (&mut self) {
-		self.ativo = false;
+// Enums também podem ter métodos!
+impl Mensagem {
+	//Este método vai processar a mensagem, dependendo do tipo que ela for.
+	fn processar(&self) {
+		//2. Usando 'match' para controlar  o fluxo baseado na variante do enum 
+		match self{
+			Mensagem::Sair => {
+				println!("Mensagem: Sair do programa.");
+			}
+			Mensagem::Mover {x , y} => {
+				println!("Movendo cursor para a posição x: {}, y:{}", x, y);
+			}
+			Mensagem::Escrever(texto) => {
+				println!("Escrevendo texto: '{}'", texto);
+			}
+			Mensagem::MudarCor(r, g, b) =>{
+				println!("Mudando a cor para (R:{}, G:{}, B:{})", r, g, b);
+			}
+			
+		}
+		
 	}
 }
 
-fn main(){
-	//2. Criando uma "instância" (um preenchimento) da nossa struct
-	// Note que a instância inteira precisa ser mútavel para alterarmos um campo.
-	let mut usuario1 = Usuario {
-		email: String::from("allan@exemplo.com"),
-		nome: String::from("Allan"),
-		ativo:true,
-		contagem_logins:1,
-	};
+fn main() {
+	//3. Criando instância das diferentes variantes do nosso enum 
+	let msg1 = Mensagem::Escrever(String::from("Olá, Parceiro de Programãção!"));
+	let msg2 = Mensagem::Mover {x: 10, y: 20};
+	let msg3 = Mensagem::MudarCor(255, 0, 128);
+	let msg4 = Mensagem::Sair;
 
-	//Chamando nosso método '.descrever()'
-	let decricao = usuario1.descrever();
-	println!("Descrição inicial: {}", decricao);
-
-	//Chamando nosso método '.desativar()' que modifica o usuário
-	usuario1.desativar();
-
-	//Verificando a mudança 
-	println!("Descrição final: {}", usuario1.descrever());
+	//4. Chamando o método para processar cada uma delas 
+	msg1.processar();
+	msg2.processar();
+	msg3.processar();
+	msg4.processar();
 }
