44c2c8082cd41a431a9a9e9bf5c40e36f7aec890
Ratatosk123 <allanmaiagondim.amg@gmail.com>
2025-08-15 20:50:15 -0300

    Aprendendo a função do Option<T>

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,30 @@
-enum Mensagem {
-	Sair,					// Varaiante sem dados 
-	Mover { x: i32, y:i32}, //Variante com uma struct anônima
-	Escrever(String),		// Variante com uma String 
-	MudarCor(i32, i32, i32), //Variante com uma tupla de 3 inteiros 
-}
-
-// Enums também podem ter métodos!
-impl Mensagem {
-	//Este método vai processar a mensagem, dependendo do tipo que ela for.
-	fn processar(&self) {
-		//2. Usando 'match' para controlar  o fluxo baseado na variante do enum 
-		match self{
-			Mensagem::Sair => {
-				println!("Mensagem: Sair do programa.");
-			}
-			Mensagem::Mover {x , y} => {
-				println!("Movendo cursor para a posição x: {}, y:{}", x, y);
-			}
-			Mensagem::Escrever(texto) => {
-				println!("Escrevendo texto: '{}'", texto);
-			}
-			Mensagem::MudarCor(r, g, b) =>{
-				println!("Mudando a cor para (R:{}, G:{}, B:{})", r, g, b);
-			}
-			
-		}
-		
+//Esta função retorna um Option contendo um f64
+//Ela encapsula a lógica de que a divisão pode falhar.
+fn dividir(numerador: f64, denominador: f64) -> Option<f64> {
+	if denominador == 0.0 {
+		None //Divisão por zero! Retornamos  ausência de valor 
+	} else {
+		Some(numerador / denominador) //Sucesso! Retornamos o resultado dentro de um Some.
 	}
 }
 
 fn main() {
-	//3. Criando instância das diferentes variantes do nosso enum 
-	let msg1 = Mensagem::Escrever(String::from("Olá, Parceiro de Programãção!"));
-	let msg2 = Mensagem::Mover {x: 10, y: 20};
-	let msg3 = Mensagem::MudarCor(255, 0, 128);
-	let msg4 = Mensagem::Sair;
+	let resultado_sucesso = dividir(10.0, 2.0);
+	let resultado_falha = dividir(10.0, 0.0);
 
-	//4. Chamando o método para processar cada uma delas 
-	msg1.processar();
-	msg2.processar();
-	msg3.processar();
-	msg4.processar();
+	//Usamos 'match' para tratar com segurança o retorno da função.
+	// O compilador nos força a tratar tanto Some quando None.
+
+	println!("Tratando o caso com sucesso:");
+	match resultado_sucesso {
+		Some(valor) => println!("O resultado é {}", valor), //Se for Some, extraímos o valor.
+		None => println!("Não foi possivel realizar a divisão."), //Se for None, mostra uma mensagem.  
+	}
+
+	println!("\nTratando o caso de falha:");
+	match resultado_falha {
+		Some(valor) => println!("O resultado é {}", valor),
+		None => println!("Não foi possivel realizar a divisão."),
+
+	}
 }
